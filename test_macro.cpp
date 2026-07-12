fc97c099a085c585716cbebd875dbbfbe9b7f3f6
TripleSec <triplesec0311@gmail.com>
2026-07-12 15:49:31 +0200

    chore: Remove accidentally committed scratch and test files

--- test_macro.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
-
-int main() {
-    std::cout << "TEST_OUTPUT: [" << STR(org.ahmoze.vector.manager) << "]" << std::endl;
-    return 0;
-}
